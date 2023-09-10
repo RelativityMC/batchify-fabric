4bbf6328f1061069377ba5e334bcafd2dadbb79a
ishland <ishlandmc@yeah.net>
2023-09-10 14:55:40 +0800

    change: use a different method for ratelimiting

--- src/main/java/com/ishland/batchify/mixin/MixinClientConnection.java
+++ src/main/java/com/ishland/batchify/mixin/MixinClientConnection.java
@@ -3,7 +3,9 @@ package com.ishland.batchify.mixin;
 import com.ishland.batchify.common.s2c.chunkupdate.ChunkUpdateConsolidator;
 import com.ishland.batchify.common.s2c.sync.SynchronizationLayer;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.flush.FlushConsolidationHandler;
+import io.netty.handler.traffic.ChannelTrafficShapingHandler;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkSide;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,10 +18,13 @@ public class MixinClientConnection {
 
     @Inject(method = "addHandlers", at = @At("RETURN"))
     private static void onAddHandlers(ChannelPipeline pipeline, NetworkSide side, CallbackInfo ci) {
-//        pipeline.addLast("LatencyOnPurpose", new LatencyOnPurpose(100L));
         pipeline.addLast("ChunkUpdateConsolidator", new ChunkUpdateConsolidator());
         pipeline.addLast("SynchronizationLayer", new SynchronizationLayer());
         pipeline.addLast(new FlushConsolidationHandler());
+        if (Boolean.getBoolean("batchify.2mbpsConnection") && pipeline.channel() instanceof SocketChannel) {
+            final ChannelTrafficShapingHandler trafficShapingHandler = new ChannelTrafficShapingHandler(2 * 1024 * 1024 / 8, 0, 1000L);
+            pipeline.addAfter("timeout", "TrafficShaping", trafficShapingHandler);
+        }
     }
 
 }
